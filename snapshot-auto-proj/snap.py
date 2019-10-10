cc2e507116154beb877d30b812b10722e105a832
pranay ashar <pranayashar@gmail.com>
2019-10-10 15:52:29 -0400

    Introduced a method to list all the ec2 instance within your account.

--- snapshot-auto-proj/snap.py
+++ snapshot-auto-proj/snap.py
@@ -1,9 +1,26 @@
 import boto3
+import click
+import sys
+
+session = boto3.Session(profile_name = 'aws-snapshot-auto')
+ec2 = session.resource('ec2')
+
+@click.command()
+def list_instances():
+    for i in ec2.instances.all():
+        print(' '.join(("ID :- ",
+            i.id,
+            "\nInstace Type :-",
+            i.instance_type,
+            "\nAvailability Zones :-",
+            i.placement['AvailabilityZone'],
+            "\nState :-",
+            i.state['Name'],
+            # i.public_dns_name
+        )))
+
+        return
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name = 'aws-snapshot-auto')
-    ec2 = session.resource('ec2')
-
-    ec2.instances.all()
-    for i in ec2.instances.all():
-        print(i)
+    print(sys.argv)
+    list_instances()
